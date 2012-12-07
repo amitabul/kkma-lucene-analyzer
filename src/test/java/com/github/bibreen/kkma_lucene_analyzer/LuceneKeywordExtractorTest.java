9659246a032672bbbd15601f548b4b1b84380c90
bibreen <bibreen@gmail.com>
2012-12-07 15:16:46 +0900

    최초 임포트

---
+++ src/test/java/com/github/bibreen/kkma_lucene_analyzer/LuceneKeywordExtractorTest.java
@@ -0,0 +1,24 @@
+package com.github.bibreen.kkma_lucene_analyzer;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LuceneKeywordExtractorTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
